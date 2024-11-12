b1ea403cbd0efc6a5445c247313981c590f47512
William So <polyipseity@gmail.com>
2024-11-12 09:40:33 +0800

    Implement `Account.java`
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/main/java/base/Account.java
+++ src/main/java/base/Account.java
@@ -1,5 +1,6 @@
 package base;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.*;
 
@@ -12,42 +13,44 @@ public class Account {
 		this.balance = balance;
 	}
 
-	// TODO: Task1 
+	// TODO: Task1
 	// replace the null with a lambda expression
-	public static Consumer<Account> add100 = null
-
-	
+	public static Consumer<Account> add100 = account -> account.balance += 100;
 
 	// TODO: Task2
 	/**
-	*	define checkBound using lowerBound and upperBound
-	 *  lowerBound is 0, upperBound is 10000
-	 *  We want checkBound to check BOTH lowerBound AND upperBound.
-	*/
-	public static Predicate<Account> lowerBound = null;
-	public static Predicate<Account> upperBound = null;
-	public static Predicate<Account> checkBound = null;
-
+	 * define checkBound using lowerBound and upperBound
+	 * lowerBound is 0, upperBound is 10000
+	 * We want checkBound to check BOTH lowerBound AND upperBound.
+	 */
+	public static Predicate<Account> lowerBound = account -> 0 <= account.balance;
+	public static Predicate<Account> upperBound = account -> account.balance <= 10000;
+	public static Predicate<Account> checkBound = lowerBound.and(upperBound);
 
 	// TODO: Task3
 	// replace the null with a lambda expression
-	// implement a public interface AddMaker that contains single abstract method make(int N)
-	// make(int N) returns a Consumer<Account> that adds N to the balance of the account
+	// implement a public interface AddMaker that contains single abstract method
+	// make(int N)
+	// make(int N) returns a Consumer<Account> that adds N to the balance of the
+	// account
 	// implements make(int N) to return a lambda expression
 	// TODO
+	@FunctionalInterface
+	public interface AddMaker {
+		Consumer<Account> make(int N);
+	}
 
 	// TODO
-	public static AddMaker maker = null;
-
+	public static AddMaker maker = N -> account -> account.balance += N;
 
 	// You can assume that all the Account in acconts have positive balances.
 	public static int getMaxAccountID(List<Account> accounts) {
-		// TODO: Task4 
+		// TODO: Task4
 		// replace the null with a stream expression
-		Account maxOne = null;
+		Account maxOne = accounts.stream().max(Comparator.comparingInt(account -> account.balance))
+				.orElseThrow(IllegalArgumentException::new);
 
 		return maxOne.id;
 	}
-
 
 }
